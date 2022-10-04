0676771e1e8b58888481e9f261a2f6344a1c267e
Mohamed Rizad <rizadvillan@gmail.com>
2022-10-04 21:04:53 +0530

    confg file

---
+++ Config.py
@@ -0,0 +1,12 @@
+from pyrogram import *
+
+api_id = "<PUT API-ID HERE>"
+api_hash = "<PUT API-HASH HERE>"
+bot_token = "<PUT BOT-TOKEN HERE>"
+
+url = "https://thispersondoesnotexist.com/image"
+
+bot=Client('AI-Faker',
+           api_id=api_id,
+           api_hash=api_hash,
+           bot_token=bot_token)
